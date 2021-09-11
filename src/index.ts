e606a07bf908a4eb6779472ef2f69fb6d6e7fce0
Ross Light <ross@zombiezen.com>
2021-09-11 09:12:44 -0700

    Switch copyright author to 'The esbuild-plugin-stimulus Authors'

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2021 Ross Light
+ * Copyright 2021 The esbuild-plugin-stimulus Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
